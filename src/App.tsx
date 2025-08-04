4b8949fb05c04357a5eb74f1e5f40e7a2b6a2dcb
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-04 14:18:26 +0530

    feat: implement main application layout with Header and Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router";
+import { lazy } from "react";
+
+import { Header } from "./components/Header";
+import { SideBar } from "./components/Sidebar";
+
+const DashboardPage = lazy(() => import("@/pages/Dashboard"));
+
 function App() {
-  return <div className="">App</div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <div className="flex">
+        <SideBar />
+        <main className="flex-1 p-8">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
