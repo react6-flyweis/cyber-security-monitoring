23e2c331bc835d79712e44867f3f1ecbcb5defbf
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-05 12:32:27 +0530

    feat: integrate AuthWrapper and AuthLayout for authentication routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router";
 import { lazy } from "react";
 import { Layout } from "@/components/Layout";
+import { AuthLayout } from "@/components/AuthLayout";
+import { AuthWrapper } from "./components/AuthWrapper";
 
+const Login = lazy(() => import("@/pages/Login"));
 const DashboardPage = lazy(() => import("@/pages/Dashboard"));
 const DevicesPage = lazy(() => import("@/pages/Devices"));
 const DetectionResponsePage = lazy(() => import("@/pages/DetectionResponse"));
@@ -9,6 +12,16 @@ const DetectionResponsePage = lazy(() => import("@/pages/DetectionResponse"));
 function App() {
   return (
       <Routes>
+        <Route
+          element={
+            <AuthWrapper>
+              <AuthLayout />
+            </AuthWrapper>
+          }
+        >
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<div>Register Page</div>} />
+        </Route>
         <Route element={<Layout />}>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/devices" element={<DevicesPage />} />
